13583f4253773f6a856c6161d6fdf923c1fc897a
Sahil Bambulkar <Sahilkbambulkar@gmail.com>
2021-06-28 16:00:40 -0400

    Fixed issue with email header

--- maildemo.py
+++ maildemo.py
@@ -45,19 +45,23 @@ allDonators = ReadCSVFile()
 
 #CREATE EMAIL WITH THOSE VALUES
 for donator in allDonators:
+	print(f'Sending email to: {donator.Name}...')
 	msg = EmailMessage()
 	msg['From'] = os.environ['PERS_EMAIL']
 	msg['To'] = donator.Email
-	print()
 	msg['Subject'] = Subject.replace('CUSTOMER_NAME',donator.Name)
 	msg.set_content(Body.replace('REPLACE_DATE',donator.Date).replace('DONATION_AMOUNT',donator.Amount).replace('DONATION_DATE',donator.Date))
-
+	#print(msg.get('Content-Type'))
+	#print(msg.get('Content-Transfer-Encoding'))
+	#print(msg.get('MIME-Version'))
+	for hdr in ['Content-Type', 'Content-Transfer-Encoding', 'MIME-Version']:
+		del msg[hdr]
 	#SEND EMAIL WITH THOSE VALUES
+	exit()
 	with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
 		smtp.login(os.environ['PERS_EMAIL'],os.environ['PERS_EMAILPW'])
 		smtp.send_message(msg)
-	exit()
-			
+		
 
 
 
